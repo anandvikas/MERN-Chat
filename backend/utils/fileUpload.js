9c388792aecb9449adfb37ac00b934f6734f005d
anandvikas <viccu7@gmail.com>
2023-05-30 20:43:44 +0530

    correction for deployment #1

--- backend/utils/fileUpload.js
+++ backend/utils/fileUpload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
-const path = require("node:path");
-const fs = require("node:fs")
+const path = require("path");
+const fs = require("fs")
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
