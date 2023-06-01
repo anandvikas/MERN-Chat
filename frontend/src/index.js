530d49935587bf85f03a8df778d23ef6a3deeb5d
anandvikas <viccu7@gmail.com>
2023-06-01 07:24:26 +0530

    added netlify.toml file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,9 +14,9 @@ import "./styles/loader.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>
+  </React.StrictMode>
 );
 
 
